e7a4db22ee6c08749f175c63303d51a3128fe2d6
saksham1242 <saksham1242.be21@chitkara.edu.in>
2024-12-09 17:37:39 +0530

    3rd

--- server/server.js
+++ server/server.js
@@ -149,6 +149,7 @@ io.on("connection", (socket) => {
       (choice1 === "rock" && choice2 === "scissors") ||
       (choice1 === "paper" && choice2 === "rock") ||
       (choice1 === "scissors" && choice2 === "paper")
+      
     ) {
       return "player1";
     }
